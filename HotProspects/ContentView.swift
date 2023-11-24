246c4839c7aaebbffa4c084cca018797c02790d4
Héctor Sandoval <ingsan@live.com.mx>
2023-11-24 09:46:39 -0600

    Day 81

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -120,19 +120,115 @@ import SwiftUI
 //    }
 //}
 
+//struct ContentView: View {
+//    var body: some View {
+//        Image("example")
+//            .interpolation(.none)
+//            .resizable()
+//            .scaledToFit()
+//            .frame(maxHeight: .infinity)
+//            .background(.black)
+//            .ignoresSafeArea()
+//    }
+//}
+
+//struct ContentView: View {
+//    @State private var backgroundColor = Color.red
+//
+//    var body: some View {
+//        VStack {
+//            Text("Hello, World!")
+//                .padding()
+//                .background(backgroundColor)
+//
+//            Text("Change Color")
+//                .padding()
+//                .contextMenu {
+//                    Button(role: .destructive) {
+//                        backgroundColor = .red
+//                    } label: {
+//                        Label("Red", systemImage: "checkmark.circle.fill")
+//                    }
+//                    Button("Green") {
+//                        backgroundColor = .green
+//                    }
+//
+//                    Button("Blue") {
+//                        backgroundColor = .blue
+//                    }
+//            }
+//        }
+//    }
+//}
+
+//struct ContentView: View {
+//    var body: some View {
+//        List {
+//            Text("Taylor Swift")
+//                .swipeActions {
+//                    Button(role: .destructive) {
+//                        print("Deleting")
+//                    } label: {
+//                        Label("Delete", systemImage: "minus.circle")
+//                    }
+//                }
+//                .swipeActions(edge: .leading) {
+//                    Button {
+//                        print("Pinning")
+//                    } label: {
+//                        Label("Pin", systemImage: "pin")
+//                    }
+//                    .tint(.orange)
+//                }
+//        }
+//    }
+//}
+
+//import UserNotifications
+//struct ContentView: View {
+//    var body: some View {
+//        VStack {
+//            Button("Request Permission") {
+//                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+//                    if success {
+//                        print("All set!")
+//                    } else if let error = error {
+//                        print(error.localizedDescription)
+//                    }
+//                }
+//            }
+//
+//            Button("Schedule Notification") {
+//                let content = UNMutableNotificationContent()
+//                content.title = "Feed the cat"
+//                content.subtitle = "It looks hungry"
+//                content.sound = UNNotificationSound.default
+//                
+//                // show this notification five seconds from now
+//                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+//                
+//                // choose a random identifier
+//                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+//                
+//                // add our notification request
+//                UNUserNotificationCenter.current().add(request)
+//            }
+//        }
+//    }
+//}
+
+import SamplePackage
 struct ContentView: View {
+    let possibleNumbers = Array(1...60)
+    var results: String {
+        let selected = possibleNumbers.random(7).sorted()
+        let strings = selected.map(String.init)
+        return strings.joined(separator: ", ")
+    }
     var body: some View {
-        Image("example")
-            .interpolation(.none)
-            .resizable()
-            .scaledToFit()
-            .frame(maxHeight: .infinity)
-            .background(.black)
-            .ignoresSafeArea()
+        Text(results)
     }
 }
-
-
 
 #Preview {
     ContentView()
